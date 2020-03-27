093c66acec6472ebbf203063ca2329775e8a801c
Mcl <mixool0204@gmail.com>
2020-03-27 20:41:06 +0800

    Update gost.sh

--- gost.sh
+++ gost.sh
@@ -4,8 +4,8 @@
 
 METHOD="-L=ss://AEAD_CHACHA20_POLY1305:$(tr -dc 'a-z0-9A-Z' </dev/urandom | head -c 16)@:$(shuf -i 10000-65535 -n1)"
 
-VER="$(wget -qO- https://github.com/ginuerzh/gost/tags | grep -oE "/tag/v[^\"]*" | head -n1 | cut -dv -f2)"
-VER=${VER:=2.9.1}
+VER="$(wget -qO- https://github.com/ginuerzh/gost/tags | grep -oEm1 "/tag/v[^\"]*" | cut -dv -f2)"
+VER=${VER:=2.11.0}
 URL="https://github.com/ginuerzh/gost/releases/download/v${VER}/gost-linux-amd64-${VER}.gz"
 
 echo "1. Downloading gost-linux-amd64-${VER}.gz to /root/gost from $URL" && echo
