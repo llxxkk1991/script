958f8b747179c0add4e359fcfd05b1ebc7910e34
Mcl <mixool0204@gmail.com>
2020-04-23 01:14:31 +0800

    latest

--- gost.sh
+++ gost.sh
@@ -4,15 +4,10 @@
 
 [[ $# != 0 ]] && METHOD=$(echo $@) || METHOD="-L=ss://AEAD_CHACHA20_POLY1305:$(tr -dc 'a-z0-9A-Z' </dev/urandom | head -c 16)@:$(shuf -i 10000-65535 -n1)"
 
-VER="$(wget -qO- https://github.com/ginuerzh/gost/tags | grep -oEm1 "/tag/v[^\"]*" | cut -dv -f2)"
-VER=${VER:=2.11.0}
-URL="https://github.com/ginuerzh/gost/releases/download/v${VER}/gost-linux-amd64-${VER}.gz"
-
-echo "1. Downloading gost-linux-amd64-${VER}.gz to /usr/bin/gost from $URL" && echo
+URL="$(wget -qO- https://api.github.com/repos/ginuerzh/gost/releases/latest | grep -E "browser_download_url.*gost-linux-amd64" | cut -f4 -d\")"
 rm -rf /usr/bin/gost
 wget -O - $URL | gzip -d > /usr/bin/gost && chmod +x /usr/bin/gost
 
-echo "2. Generate /lib/systemd/system/gost.service"
 cat <<EOF > /lib/systemd/system/gost.service
 [Unit]
 Description=gost
@@ -25,4 +20,3 @@ WantedBy=multi-user.target
 EOF
 
 systemctl enable gost.service && systemctl daemon-reload && systemctl restart gost.service && systemctl status gost
-
