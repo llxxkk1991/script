28f38e60b3f71ca65a3fa943f9b8e4dc2dc1f24a
Mcl <mixool0204@gmail.com>
2020-06-03 13:49:35 +0800

    Update nft.sh

--- nft.sh
+++ nft.sh
@@ -16,10 +16,6 @@ table inet filter {
         ct state established,related accept
         tcp dport { 80, 443 } accept
         tcp dport $(cat /etc/ssh/sshd_config | grep -oE "^Port [0-9]*$" | grep -oE "[0-9]*" || echo 22) ct state new limit rate 5/minute accept
-        ip protocol icmp icmp type echo-request limit rate 20 bytes/second burst 500 bytes accept
-        ip protocol icmp icmp type echo-request drop
-        ip protocol icmp icmp type { destination-unreachable, router-advertisement, router-solicitation, time-exceeded, parameter-problem } accept
-        ip protocol igmp accept
     }
 
     chain forward {
