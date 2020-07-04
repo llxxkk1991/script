c2f028f08f7fd404d6e6a1634f14887d4e058cd1
Mcl <mixool0204@gmail.com>
2020-07-04 11:54:55 +0800

    Create pingtunnel.sh

---
+++ pingtunnel.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# Wiki: https://github.com/esrrhs/pingtunnel
+# Usage: bash <(curl -s https://raw.githubusercontent.com/mixool/script/debian-9/pingtunnel.sh) -type server
+# Uninstall: systemctl stop pingtunnel; systemctl disable pingtunnel; rm -rf /etc/systemd/system/pingtunnel.service /usr/bin/pingtunnel
+
+[[ $# != 0 ]] && METHOD=$(echo $@) || METHOD="-type server"
+
+URL="$(wget -qO- https://api.github.com/repos/esrrhs/pingtunnel/releases/latest | grep -E "browser_download_url.*pingtunnel_linux64" | cut -f4 -d\")"
+rm -rf /usr/bin/pingtunnel
+wget -O - $URL | gzip -d > /usr/bin/pingtunnel && chmod +x /usr/bin/pingtunnel
+
+https://github.com/esrrhs/pingtunnel/releases/download/2.3/pingtunnel_linux64.zip
+
+cat <<EOF > /etc/systemd/system/pingtunnel.service
+[Unit]
+Description=pingtunnel
+[Service]
+ExecStart=/usr/bin/pingtunnel $METHOD
+Restart=always
+User=root
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl enable pingtunnel.service && systemctl daemon-reload && systemctl restart pingtunnel.service && systemctl status pingtunnel
