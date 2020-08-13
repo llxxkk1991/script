13633bcc23894767551d1b95fce65ed02086aec3
Mcl <mixool0204@gmail.com>
2020-08-13 20:44:30 +0800

    https://github.com/klzgrad/naiveproxy/issues/110

--- gost-acme-https.sh
+++ gost-acme-https.sh
@@ -2,7 +2,7 @@
 # Wiki: https://docs.ginuerzh.xyz/gost/
 # Usage: bash <(curl -s https://raw.githubusercontent.com/mixool/script/debian-9/gost-acme-https.sh) my.domain.com CF_Key CF_Email
 ## 一键GOST搭建443端口的服务端HTTPS代理，并开启防探测。使用acme和cloudflareApi自动管理证书，重复运行即可更改随机账号密码
-## SwithcyOmega使用这个代理，先访问一次knock这个网址,推荐客户端naiveproxy: https://github.com/klzgrad/naiveproxy
+## SwithcyOmega使用这个代理，先访问一次knock这个网址
 ## Uninstall: /root/.acme.sh/acme.sh --uninstall; systemctl stop gost; systemctl disable gost; rm -rf /etc/systemd/system/gost.service /usr/bin/gost /etc/gost
 
 ######## 脚本需要传入三个参数： 域名,Cloudflare账户的GobalAPI,Cloudflare账户的Email
