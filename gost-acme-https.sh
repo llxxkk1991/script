f9cbc456b6d3725df617a2ec906ea6b668ad10b1
Mcl <mixool0204@gmail.com>
2020-08-13 10:52:12 +0800

    Update gost-acme-https.sh

--- gost-acme-https.sh
+++ gost-acme-https.sh
@@ -1,41 +1,49 @@
 #!/usr/bin/env bash
-# Wiki: https://docs.ginuerzh.xyz/gost/ && https://github.com/acmesh-official/acme.sh/wiki
-# Usage: bash <(curl -s https://raw.githubusercontent.com/mixool/script/debian-9/gost-acme-https.sh)
-## 使用GOST搭建443端口的服务端HTTPS代理，并开启防探测。使用acme自动申请证书，只在申请和更新证书时acme占用80端口。重复运行即可更改配置。
-### 卸载： systemctl disable gost && systemctl stop gost && rm -rf /etc/gost /usr/bin/gost /etc/systemd/system/gost.service && /root/.acme.sh/acme.sh --uninstall
+# Wiki: https://docs.ginuerzh.xyz/gost/
+# Usage: bash <(curl -s https://raw.githubusercontent.com/mixool/script/debian-9/gost-acme-https.sh.sh) my.domain.com CF_Key CF_Email
+## 一键GOST搭建443端口的服务端HTTPS代理，并开启防探测。使用acme和cloudflareApi自动管理证书，重复运行即可更改随机账号密码。
+## Uninstall: /root/.acme.sh/acme.sh --uninstall; systemctl stop gost; systemctl disable gost; rm -rf /etc/systemd/system/gost.service /usr/bin/gost
 
-read -p "Please input your domain name for vps:" domain
-read -p "Please input your username:" username
-read -p "Please input your password:" password
-read -p "Please input your probe_resist:" probe_resist
+######## 脚本需要传入三个参数： 域名,Cloudflare账户的GobalAPI,Cloudflare账户的Email
+[[ $# != 3 ]] && echo Err !!! Useage: bash this_script.sh my.domain.com CF_Key CF_Email && exit 1
+domain="$1"
+export CF_Key="$2"
+export CF_Email="$3"
+########
 
-#### 获取最新版gost并配置gost.service
+# install acme.sh
+apt install socat -y
+curl https://get.acme.sh | sh
+source  ~/.bashrc
+/root/.acme.sh/acme.sh --issue --dns dns_cf --keylength ec-256 -d $domain
+rm -rf /etc/gost; mkdir -p /etc/gost
+/root/.acme.sh/acme.sh --installcert -d $domain --ecc --fullchain-file /etc/gost/gost.crt --key-file /etc/gost/gost.key --reloadcmd "service gost restart"
+
+# install gost
 URL="$(wget -qO- https://api.github.com/repos/ginuerzh/gost/releases/latest | grep -E "browser_download_url.*gost-linux-amd64" | cut -f4 -d\")"
 rm -rf /usr/bin/gost
 wget -O - $URL | gzip -d > /usr/bin/gost && chmod +x /usr/bin/gost
+
+## 探测防御使用caddy2默认页面
+wget -O /etc/gost/index.html https://raw.githubusercontent.com/mixool/script/source/index.html
+
+## 代理账号密码以及Knock参数: https://docs.ginuerzh.xyz/gost/probe_resist/
+username="$(tr -dc 'a-z0-9A-Z' </dev/urandom | head -c 16)"
+password="$(tr -dc 'a-z0-9A-Z' </dev/urandom | head -c 16)"
+knock="$username.$password"
 
 cat <<EOF > /etc/systemd/system/gost.service
 [Unit]
 Description=gost
 [Service]
-ExecStart=/usr/bin/gost -L=http2://$username:$password@:443?probe_resist=web:$probe_resist&knock=www.google.com&cert=/etc/gost/gost.crt&key=/etc/gost/gost.key
+ExecStart=/usr/bin/gost -L=https://$username:$password@:443?probe_resist=file:/etc/gost/index.html&knock=$knock&cert=/etc/gost/gost.crt&key=/etc/gost/gost.key
 Restart=always
 User=root
 [Install]
 WantedBy=multi-user.target
 EOF
 
-#### 使用acme自动签发和安装证书
-apt update && apt install socat -y
-curl https://get.acme.sh | sh
+systemctl enable gost.service && systemctl daemon-reload && systemctl restart gost.service && systemctl status gost | more | grep -A 2 "gost.service"
 
-/root/.acme.sh/acme.sh  --upgrade  --auto-upgrade
-
-/root/.acme.sh/acme.sh --issue -d $domain --standalone
-
-mkdir -p /etc/gost
-
-/root/.acme.sh/acme.sh --install-cert -d $domain --key-file /etc/gost/gost.key --fullchain-file /etc/gost/gost.crt --reloadcmd "systemctl restart gost"
-
-#### 启动gost.service
-systemctl enable gost.service && systemctl daemon-reload && systemctl restart gost.service && sleep 3 && systemctl status gost
+# info
+echo; echo $(date); echo knock: $knock; echo username: $username; echo password: $password; echo https://$domain
